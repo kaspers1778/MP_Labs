04f5ab688ffb991fb7b65d0527e04f81e07fd570
Pete Balachin <pet.balachin1778@gmail.com>
2020-12-24 07:58:02 +0200

    Update lab4.pl

--- L_4/lab4.pl
+++ L_4/lab4.pl
@@ -4,7 +4,6 @@
 neighbor(X, Y, List) :- nextto(X, Y, List).
 neighbor(X, Y, List) :- nextto(Y, X, List) .
 where_is_fish(R) :-
-   /* 0. Всего 5 домов */
     Houses = [_,_,_,_,_],
     nth1(1, Houses, [norwegian,_,_,_,_]),
     member([englishman,_,_,_,red], Houses),
